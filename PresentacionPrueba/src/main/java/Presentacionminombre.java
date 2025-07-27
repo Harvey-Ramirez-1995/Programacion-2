d9b0be84b83eeb010451a48824721c3c10a98b3e
Harvey-Ramirez-1995 <hramirezb3@miumg.edu.gt>
2025-07-26 23:55:11 -0600

    Prueba Presentacion
    ejercicio que dejo el Ing para ver si podemos imprimir quienes somos en vez del hola mundo

---
+++ PresentacionPrueba/src/main/java/Presentacionminombre.java
@@ -0,0 +1,5 @@
+public class Presentacionminombre {
+    public static void main(String[] args) {
+    System.out.println("!Hola soy Harvey Ramirez! Estudio Ingenieria en Sistemas, 4to semestre.");
+}
+}
