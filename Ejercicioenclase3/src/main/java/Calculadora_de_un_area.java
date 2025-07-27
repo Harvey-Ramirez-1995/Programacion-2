db9a25b6379cdf31204a40354220fd5b6bcafe60
Harvey-Ramirez-1995 <hramirezb3@miumg.edu.gt>
2025-07-27 11:48:43 -0600

    Clase no. 3
    Clase 3 y ejercicios

---
+++ Ejercicioenclase3/src/main/java/Calculadora_de_un_area.java
@@ -0,0 +1,8 @@
+public class Calculadora_de_un_area {
+    public static void main(String[] args) {
+        double radio = 4.7;
+        double area = Math.PI * radio * radio;
+
+        System.out.println("El Area del circulo es: " + area);
+    }
+}
